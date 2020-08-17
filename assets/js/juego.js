0bcce6a667647cd093c53348ab08859c10fcce06
BetoSandoval <alberto.sa1391@gmail.com>
2020-08-16 23:48:47 -0500

    Funcion pedir carta agregada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'k'];
 
+// Esta funcion crea una nueva baraja
 const crearDeck = () => {
 
     for( let i = 2; i<= 10; i++ ){
@@ -23,9 +24,26 @@ const crearDeck = () => {
         }
     }
 
-    console.log( deck );
+    // console.log( deck );
     deck = _.shuffle( deck );
     console.log( deck );
+    return deck;
 }
 
 crearDeck();
+
+// Esta funcion permite tomar una carta
+const pedirCarta = () => {
+
+    if (deck.length === 0){
+        throw 'No hay cartas en el deck';
+    }
+
+    carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta);//  carta debe de ser de la baraja
+    return carta;
+}
+
+pedirCarta();
